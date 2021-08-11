5dfc297646637f9e8093d81a11c9290e636c6b7d
Rahul Roy <70642284+Rahul171201@users.noreply.github.com>
2021-08-11 12:34:13 +0530

    added files

---
+++ app.js
@@ -0,0 +1,120 @@
+//jshint esversion:6
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const ejs = require("ejs");
+const lodash = require("lodash");
+
+const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
+const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
+const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
+
+const app = express();
+
+app.set('view engine', 'ejs');
+
+var mongoDB = 'mongodb://127.0.0.1/postDB';
+mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
+
+//Get the default connection
+var db = mongoose.connection;
+
+//Bind connection to error event (to get notification of connection errors)
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
+const postSchema = {
+  postTitle: String,
+  postBody: String
+}
+const Post = mongoose.model("post", postSchema);
+
+
+let posts = [];
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+
+
+app.get('/', function (req, res) {
+  Post.find({}, function (err, result) {
+    if (err) {
+      console.log(err);
+    }
+    else {
+      
+      result.forEach(function (element) {
+        let flag = 0;
+        for (var i = 0; i < posts.length; i++) {
+          if (element === posts[i]) {
+            flag = 1;
+          }
+        }
+        if (flag === 0)
+          posts.push(element);
+      });
+    }
+  });
+  res.render('home', { content: homeStartingContent, postList: posts });
+  // posts = [];
+});
+app.get('/about', function (req, res) {
+  res.render('about', { content: aboutContent });
+});
+app.get('/contact', function (req, res) {
+  res.render('contact', { content: contactContent });
+});
+app.get('/compose', function (req, res) {
+  res.render('compose');
+});
+
+app.post('/compose_button', function (req, res) {
+  res.redirect('/compose');
+});
+
+app.get('/delete', function (req, res) {
+  Post.deleteMany({}, function (err, result) {
+    if (!err) {
+      console.log("Successfully Deleted");
+    }
+    else {
+      console.log(err);
+    }
+  });
+  res.redirect('/');
+  posts = [];
+});
+
+app.post('/delete_button', function (req, res) {
+  res.redirect('/delete');
+});
+
+app.get('/posts/:postTopic', function (req, res) {
+  posts.forEach(function (element) {
+    const topic = lodash.lowerCase(req.params.postTopic);
+    if (lodash.lowerCase(element.postTitle) === topic) {
+      res.render('post', { postTitle: element.postTitle, postBody: element.postBody });
+      console.log("Match Found");
+    }
+  });
+});
+
+app.post('/compose', function (req, res) {
+  // let compose = {
+  //   postBody: req.body.compose_body,
+  //   postTitle: req.body.compose_title
+  // }
+  const newPost = new Post({
+    postTitle: req.body.compose_title,
+    postBody: req.body.compose_body
+  });
+  newPost.save();
+  res.redirect('/');
+});
+
+
+
+app.listen(3000, function () {
+  console.log("Server started on port 3000");
+});
